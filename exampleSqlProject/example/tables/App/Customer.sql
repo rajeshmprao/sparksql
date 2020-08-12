7274622ad65f1c7c58cf6381fbedecd1ef76cc77
sahemant <16522686+sahemant@users.noreply.github.com>
2020-08-12 11:09:00 +0530

    Added newColumn in App.Customer

--- exampleSqlProject/example/tables/App/Customer.sql
+++ exampleSqlProject/example/tables/App/Customer.sql
@@ -1,9 +1,10 @@
 CREATE TABLE App.Customer
 (
     Id int not null,
-    name string comment 'awesome name',
+    name string,
     phonenumber string,
-    haha string
+    haha string,
+    newColumn int comment 'this is really great'
 )
 USING DELTA
 LOCATION '$LAKE_PATH/App/Customer'
